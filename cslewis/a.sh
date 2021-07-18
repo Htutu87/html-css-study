21a9a9eaf06dac2efcb150149ef032b53c1765cd
Htutu87 <artur_amaral@poli.ufrj.br>
2021-07-18 12:40:18 -0300

    Organização dos repositórios.

---
+++ cslewis/a.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ cp index.html about.html 
+ cp index.html lewis.html 
+ cp index.html give.html 
+ cp index.html events.html 
+ cp index.html webinars.html 
+ cp index.html kilns.html 
+ cp index.html videos.html  
+ cp index.html media.html 
+ cp index.html contact.html
+
